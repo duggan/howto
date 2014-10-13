1a084197bfb346acc811222e74917e65bb1b8e84
Ross Duggan <rduggan@engineyard.com>
2014-10-13 16:26:13 +0100

    mage_run_code

--- public/index.php
+++ public/index.php
@@ -26,6 +26,7 @@
 					echo "<p>Your app is running on PHP version: " . $version . "<br/></p>";
 					echo "<p>The app IP is: " . $_SERVER['SERVER_ADDR'] . "<br/></p>";
 					echo "<p>The client IP is : " . $_SERVER['HTTP_X_FORWARDED_FOR'] . "<br/></p>";
+					echo "<p>The Magento site is : " . $_SERVER['MAGE_RUN_CODE'] . "<br/></p>";
 					echo "<p>Temp dir available to your app is: " . sys_get_temp_dir() . "</p>";
                 ?>
                 <?php
